e754bbdefa41027c0cdb9dd00699c2d5f08dfd3c
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-14 20:02:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+            //base
+            Console.Write("informe a base:");
+            b = int.Parse(Console.ReadLine());
+            //altura
+            Console.Write("informe a altura:");
+            h = int.Parse(Console.ReadLine());
+            //resultado
+            a = b * h;
+            Console.WriteLine("valor da area {0}", a);
+        }
+    }
+}
